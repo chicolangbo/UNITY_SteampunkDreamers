a46c7b9674db032a9f957789cc2d56988d72ec6e
CHICOLANGBO\minji <minji4885@gmail.com>
2023-10-16 00:37:17 +0900

    - applying assets

--- SteampunkDreamers/Assets/Scripts/UIControllers/SpeedBarController.cs
+++ SteampunkDreamers/Assets/Scripts/UIControllers/SpeedBarController.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class SpeedBarController : MonoBehaviour
 {
     public Image fillBar;
-    public RectTransform arrow;
     private PlayerController playerController;
     public float value = 0;
     public float accelerator;
@@ -25,10 +24,10 @@ public class SpeedBarController : MonoBehaviour
 
     public void SpeedChange(float value)
     {
-        if (value > 100 || value < 0)
+        if (value > 1 || value < 0)
             return;
 
-        float amount = (value / 100.0f) * 180.0f / 360;
+        float amount = (value / 1f) * 180.0f / 360;
         fillBar.fillAmount = amount;
 
         float controllerAngle = amount * 360;
@@ -41,9 +40,9 @@ public class SpeedBarController : MonoBehaviour
         {
             // 1.5초 도달
             float tempValue = value;
-            value = Mathf.Clamp(tempValue + Time.deltaTime * controllSpeed, 0, 100f);
+            value = Mathf.Clamp(tempValue + Time.deltaTime * controllSpeed, 0, 1f);
 
-            if (value >= 100f)
+            if (value >= 1f)
             {
                 toRight = false;
             }
@@ -52,7 +51,7 @@ public class SpeedBarController : MonoBehaviour
         else
         {
             float tempValue = value;
-            value = Mathf.Clamp(tempValue - Time.deltaTime * controllSpeed, 0, 100f);
+            value = Mathf.Clamp(tempValue - Time.deltaTime * controllSpeed, 0, 1f);
 
             if (value <= 0)
             {
@@ -67,15 +66,15 @@ public class SpeedBarController : MonoBehaviour
         // value 0~70 : 최대 속력의 50%
         // value 70~80, 90~100 : 최대 속력의 70%
         // vlaue 80~90 : 최대 속력의 90%
-        if (value < 70)
+        if (value < 0.7)
         {
             playerController.initialSpeed = playerController.maxSpeed * 0.5f;
         }
-        else if ((value >= 70 && value < 80) || (value >= 90 && value <= 100))
+        else if ((value >= 0.7 && value < 0.8) || (value >= 0.9 && value <= 1))
         {
             playerController.initialSpeed = playerController.maxSpeed * 0.7f;
         }
-        else if (value >= 80 && value < 90)
+        else if (value >= 0.8 && value < 0.9)
         {
             playerController.initialSpeed = playerController.maxSpeed * 0.9f;
         }
